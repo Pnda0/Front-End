0e965fa2b3e87076cef19b7c0ebc49a2f496b95c
Pnda0 <alan_carvalho2015@hotmail.com>
2022-09-13 12:43:31 -0300

    gg

--- Pagina de login/config.php
+++ Pagina de login/config.php
@@ -6,7 +6,7 @@
     $port = '5432';
     $dbnome = 'Login AD Promotora';
 
-   $conexao = pg_connect("host=localhost port=5432 dbname=Login AD Promotora user=postgres password=Andromeda#8352");
+   $conexao = pg_connect("host=localhost port=5432 dbname=Login AD Promotora user=postgres password=dark_souls");
 
    if ($conexao)
    {
