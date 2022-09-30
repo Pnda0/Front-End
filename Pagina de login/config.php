d3479be68f6919b530ca3767a38cac148555cf7f
Pnda0 <alan_carvalho2015@hotmail.com>
2022-09-30 11:32:41 -0300

    gg

--- Pagina de login/config.php
+++ Pagina de login/config.php
@@ -1,11 +1,12 @@
 <?php
-    $pdo = new PDO("pgsql:dbname=ad;host=localhost user=postgres password=admin port=5432");
+    $conexao = new PDO("pgsql:dbname=ad;host=localhost user=postgres password=admin port=5432");
 
-    //catch (PDOException $e){
+    // catch (PDOException $e){
     //    echo "erro com banco de dados".$e->getMessage();
     //}
     //catch (Exception $e){
     //    echo "erro muito doido".$e->getMessage();
     //}
 
+    
 ?>
