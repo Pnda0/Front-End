5aec0caf8474126efdd163eaf7d821f4a8e4c372
Pnda0 <alan_carvalho2015@hotmail.com>
2022-10-05 10:38:58 -0300

    Vai dar certo po

---
+++ Projeto Final/Menu/alan.js
@@ -0,0 +1,23 @@
+const body = document.querySelector("body"),
+    sidebar = body.querySelector(".sidebar"),
+    toggle = body.querySelector(".toggle"),
+    searchBtn = body.querySelector(".search-box"),
+    modeSwitch = body.querySelector(".toggle-switch"),
+    modeText = body.querySelector(".mode-text");
+
+    toggle.addEventListener("click", () => {
+        sidebar.classList.toggle("close");
+    });
+
+    modeSwitch.addEventListener("click", () => {
+        body.classList.toggle("dark");
+
+        if(body.classList("dark")){
+            modeText.innerText = "Light Mode"
+        }
+        else{
+            modeText.innerText = "Dark Mode"
+        }
+    });
+
+    
---
+++ Projeto Final/php/config.php
@@ -0,0 +1,17 @@
+<?php
+    $dbHost = 'Localhost';
+    $dbUsername = 'root';
+    $dbPassword = 'admin';
+    $dbName = 'formulario';
+
+    $conexao = new mysqli($dbHost,$dbUsername, $dbPassword, $dbName);
+
+    if($conexao->connect_errno)
+    {
+        echo "Erro";
+    }
+    else
+    {
+        echo "Conexão efetuada com sucesso";
+    }
+?>
